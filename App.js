121dee74db5314f0775deb367a7e855a33f82382
Dean Williams <dean.williams4@Imp.net>
2018-11-19 16:32:28 +0000

    initial backend code. got local mongodb running successfully with some dummy code

--- App.js
+++ App.js
@@ -9,21 +9,48 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Datastore from 'react-native-local-mongodb';
+/*
+var doc = {
+	date: '19-11-2018',
+	startTime: '08:00',
+	endTime: '16:00',
+	worked: 8
+};*/
 
 type Props = {};
 export default class App extends Component<Props> {
+	constructor(props){
+		super(props);
+		
+		this.db = new Datastore({filename: 'timesheet',autoload:true});
+		
+		this.state = {
+			data: []
+		}
+
+			
+	}
+	
+	componentWillMount(){
+		//this.db.insert(doc, function(err,newDoc){});
+
+		this.db.find({date:'19-11-2018'},(err,docs)=>{
+			console.log(docs);
+			this.setState({data:docs[0]});
+		});
+	}
+
+	
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>Date: {this.state.data.date}</Text>
+        <Text style={styles.instructions}>Start Time: {this.state.data.startTime}</Text>
+        <Text style={styles.instructions}>End Time: {this.state.data.endTime}</Text>
+        <Text style={styles.instructions}>Hours Worked: {this.state.data.worked}</Text>
       </View>
     );
   }
